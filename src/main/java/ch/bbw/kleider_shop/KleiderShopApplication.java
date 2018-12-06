ae6ea8b10b5eb0fb8f11713d3946558632ed1947
5ia17aastampa <5ia17aastampa@gibwintra.ch>
2018-12-06 09:23:49 +0100

    setup project

---
+++ src/main/java/ch/bbw/kleider_shop/KleiderShopApplication.java
@@ -0,0 +1,12 @@
+package ch.bbw.kleider_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KleiderShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KleiderShopApplication.class, args);
+	}
+}
